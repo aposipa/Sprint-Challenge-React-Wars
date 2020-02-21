cd698deaa6da8cddf6db754a90c5b56dbdcca9a2
aposipa <andrew.posipanka@gmail.com>
2020-02-21 13:41:44 -0500

    Most of MVP goals reached, retreived data from API and styled it.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+
+//imports
+import StarWars from "./components/StarWarsPeople";
 import './App.css';
+import { Jumbotron, Button} from 'reactstrap';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +14,32 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://swapi.co/api/people/")
+      .then(response => {
+        console.log("this is api call log:", response.data.results);
+        setData(response.data.results);
+      })
+      .catch(error => {
+        console.log("Oops error here:", error);
+      })
+  }, [])
+
   return (
+    
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <Jumbotron>
+    <h1 className="display-3">Hello, A Galaxy Far Far Away!</h1>
+        <p className="lead">This is a list of some of the most important people in the Star Wars Universe.</p>
+        <hr className="my-2" />
+        <p>It uses an API for characters from the Episode IV A New Hope.</p>
+       
+        </Jumbotron>
+      {data.map(characters => (
+        <StarWars data = {characters}/>
+      ))}
     </div>
   );
 }
